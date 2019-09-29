72f070a02b195b2a85e7908a7a836bdd8cb62772
nusic1 <nxu6202@rit.edu>
2019-09-29 09:07:53 +0200

    Google Login

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import {Header} from "../components/Header";
 import { GoogleLogin } from 'react-google-login';
-import {GoogleAPI} from 'react-google-oauth'
+import { GoogleLogout } from 'react-google-login';
 
 export function Login(props) {
 
     const responseGoogle = (response) => {
+        console.log(response.Zi.access_token);
+        const token = response.Zi.access_token;
+        localStorage.setItem('accessToken', token);
+    };
+
+    const logout = (response) => {
         console.log(response);
+        localStorage.setItem('accessToken', '');
     };
 
     return (
         <div>
             <Header />
-            <GoogleAPI clientId="827593341333-7u9kvo2hav67polo622mmg9u1vlo8ta7.apps.googleusercontent.com"
-                       onUpdateSigninStatus={Function}
-                       onInitFailure={Function} >
-            </GoogleAPI>
+            <GoogleLogin
+                clientId="827593341333-t058si6pfn8t6j7fcdnt44dk9ugup3lq.apps.googleusercontent.com"
+                buttonText="Login"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
+            <GoogleLogout
+                clientId="827593341333-t058si6pfn8t6j7fcdnt44dk9ugup3lq.apps.googleusercontent.com"
+                buttonText="Logout"
+                onLogoutSuccess={responseGoogle}
+            >
+            </GoogleLogout>
         </div>
     );
 }
